7770f65063237cad910cd45b7892b007fc028e36
kellynauert <kellynauert@gmail.com>
2020-12-17 21:40:26 -0500

    first comment

---
+++ index.js
@@ -0,0 +1,10 @@
+document.querySelectorAll(".card").forEach((item) => {
+  item.addEventListener("mouseover", (mouseInFunc) => {
+    item.classList.add("class", "mouse-over");
+    item.classList.remove("class", "mouse-out");
+  });
+  item.addEventListener("mouseout", (mouseOutFunc) => {
+    item.classList.remove("class", "mouse-over");
+    item.classList.add("class", "mouse-out");
+  });
+});
